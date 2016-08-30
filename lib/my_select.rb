ab5501f9f23ea87483102236df616c52c482dadf
slindquist3 <slindquist3@gmail.com>
2016-08-30 17:03:32 -0400

    Built my-select

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  counter = 0
+  new_collection = []
+ while counter < collection.length
+   if yield(collection[counter]) == true
+     new_collection << collection[counter]
+   end
+   counter += 1
+ end
+ new_collection
 end
